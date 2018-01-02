03056c0d0f1e9ae4baf609e47ac0f04b407600c6
nvms <jon@pye.rs>
2018-01-02 14:49:54 -0800

    added minify-css task

--- basecoat-css/Gulpfile.js
+++ basecoat-css/Gulpfile.js
@@ -2,8 +2,22 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var sassdoc = require('sassdoc');
+
+gulp.task('minify-css', function() {
+  return gulp.src(['css/*.css', '!css/**/*.min.css'])
+    .pipe(cleanCSS({compatibility: 'ie8', debug: true}, function(details) {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css'));
+});
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
